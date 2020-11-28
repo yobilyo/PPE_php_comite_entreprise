fe20394c89deda7266f047af56636f20d2f53679
yobilyo <bbb.yousfi@gmail.com>
2020-11-28 19:44:26 +0100

    correction JMerise héritage

--- sql/comite_entreprise.sql
+++ sql/comite_entreprise.sql
@@ -12,31 +12,31 @@ use ce;
 
 CREATE TABLE Tresorerie(
         id_tresorerie Int  Auto_increment  NOT NULL ,
-        fonds         Float NOT NULL
-	,CONSTRAINT Tresorerie_PK PRIMARY KEY (id_tresorerie)
-)ENGINE=InnoDB;
+        fonds         Float,
+		PRIMARY KEY (id_tresorerie)
+);
 
-
+insert into tresorerie values (NULL, 15000);
 #------------------------------------------------------------
 # Table: activite
 #------------------------------------------------------------
 
 CREATE TABLE activite(
         id_activite   Int  Auto_increment  NOT NULL ,
-        nom           Varchar (50) NOT NULL ,
-        lieu          Varchar (50) NOT NULL ,
-        nb_personnes  Int NOT NULL ,
-        budget        Float NOT NULL ,
-        description   Varchar (200) NOT NULL ,
-        date_debut    Date NOT NULL ,
-        date_fin      Date NOT NULL ,
-        prix          Float NOT NULL ,
-        id_tresorerie Int NOT NULL
-	,CONSTRAINT activite_PK PRIMARY KEY (id_activite)
+        nom           Varchar (50),
+        lieu          Varchar (50),
+        nb_personnes  Int(5),
+        budget        Float,
+        description   Varchar (200),
+        date_debut    Date,
+        date_fin      Date,
+        prix          Float,
+        id_tresorerie Int NOT NULL,
+		PRIMARY KEY (id_activite),
+		FOREIGN KEY (id_tresorerie) REFERENCES Tresorerie(id_tresorerie)
+);
 
-	,CONSTRAINT activite_Tresorerie_FK FOREIGN KEY (id_tresorerie) REFERENCES Tresorerie(id_tresorerie)
-)ENGINE=InnoDB;
-
+insert into activite values (NULL, "Parc Asterix", "Pailly", 200, 500, "Prix casse", "2020-11-28", "2020-11-30", 25, 1);
 
 #------------------------------------------------------------
 # Table: utilisateur
@@ -44,54 +44,47 @@ CREATE TABLE activite(
 
 CREATE TABLE utilisateur(
         idutilisateur Int  Auto_increment  NOT NULL ,
-        username      Varchar (20) NOT NULL ,
-        password      Varchar (20) NOT NULL ,
-        email         Varchar (20) NOT NULL
-	,CONSTRAINT utilisateur_PK PRIMARY KEY (idutilisateur)
-)ENGINE=InnoDB;
+        username      Varchar (20),
+        password      Varchar (20),
+        email         Varchar (20),
+		PRIMARY KEY (idutilisateur)
+);
 
 
+
+insert into utilisateur values (NULL, "Melanie", "45D4E", "melanie@cfa.fr"), (NULL, "Julien", "885DE", "julien@cfa.fr"), (NULL, "Gerard", "8555ed", "Gerard@cfa.fr");
 #------------------------------------------------------------
 # Table: salarie
 #------------------------------------------------------------
 
 CREATE TABLE salarie(
         idutilisateur Int NOT NULL ,
-        nom           Varchar (20) NOT NULL ,
-        prenom        Varchar (20) NOT NULL ,
-        tel           Varchar (15) NOT NULL ,
-        adresse       Varchar (50) NOT NULL ,
-        quotient_fam  Int NOT NULL ,
-        service       Enum ("comptabilite","developpeur","commercial","ressources_humaines") NOT NULL ,
-        sexe          Enum ("homme","femme") NOT NULL ,
-        username      Varchar (20) NOT NULL ,
-        password      Varchar (20) NOT NULL ,
-        email         Varchar (20) NOT NULL
-	,CONSTRAINT salarie_PK PRIMARY KEY (idutilisateur)
+        nom           Varchar (20),
+        prenom        Varchar (20),
+        tel           Varchar (15),
+        adresse       Varchar (50),
+        quotient_fam  Int,
+        service       Enum ("comptabilite","developpeur","commercial","ressources_humaines"),
+        sexe          Enum ("homme","femme"),
+		FOREIGN KEY (idutilisateur) REFERENCES utilisateur(idutilisateur)
+);
 
-	,CONSTRAINT salarie_utilisateur_FK FOREIGN KEY (idutilisateur) REFERENCES utilisateur(idutilisateur)
-)ENGINE=InnoDB;
-
-
+insert into salarie values (1, "Melanie", "DUVIL", "0633928562", "paris", 2, "developpeur", "femme");
 #------------------------------------------------------------
 # Table: Sponsor
 #------------------------------------------------------------
 
 CREATE TABLE Sponsor(
         idutilisateur Int NOT NULL ,
-        societe       Varchar (20) NOT NULL ,
-        budget        Float NOT NULL ,
-        tel           Varchar (15) NOT NULL ,
-        username      Varchar (20) NOT NULL ,
-        password      Varchar (20) NOT NULL ,
-        email         Varchar (20) NOT NULL ,
-        id_tresorerie Int NOT NULL
-	,CONSTRAINT Sponsor_PK PRIMARY KEY (idutilisateur)
+        societe       Varchar (20),
+        budget        Float,
+        tel           Varchar (15),
+        id_tresorerie Int NOT NULL,
+		FOREIGN KEY (idutilisateur) REFERENCES utilisateur(idutilisateur),
+		FOREIGN KEY (id_tresorerie) REFERENCES Tresorerie(id_tresorerie)
+);
 
-	,CONSTRAINT Sponsor_utilisateur_FK FOREIGN KEY (idutilisateur) REFERENCES utilisateur(idutilisateur)
-	,CONSTRAINT Sponsor_Tresorerie0_FK FOREIGN KEY (id_tresorerie) REFERENCES Tresorerie(id_tresorerie)
-)ENGINE=InnoDB;
-
+insert into sponsor values (2, "IMACInfo", 8000, "0685549655", 1);
 
 #------------------------------------------------------------
 # Table: commentaire
@@ -99,15 +92,14 @@ CREATE TABLE Sponsor(
 
 CREATE TABLE commentaire(
         id_commentaire Int  Auto_increment  NOT NULL ,
-        datecomment    Date NOT NULL ,
-        contenu        Text NOT NULL ,
+        datecomment    Date,
+        contenu        Text,
         id_activite    Int NOT NULL ,
-        idutilisateur  Int NOT NULL
-	,CONSTRAINT commentaire_PK PRIMARY KEY (id_commentaire)
-
-	,CONSTRAINT commentaire_activite_FK FOREIGN KEY (id_activite) REFERENCES activite(id_activite)
-	,CONSTRAINT commentaire_salarie0_FK FOREIGN KEY (idutilisateur) REFERENCES salarie(idutilisateur)
-)ENGINE=InnoDB;
+        idutilisateur  Int NOT NULL,
+		PRIMARY KEY (id_commentaire),
+		FOREIGN KEY (id_activite) REFERENCES activite(id_activite),
+		FOREIGN KEY (idutilisateur) REFERENCES salarie(idutilisateur)
+);
 
 
 #------------------------------------------------------------
@@ -116,20 +108,11 @@ CREATE TABLE commentaire(
 
 CREATE TABLE admin(
         idutilisateur Int NOT NULL ,
-        username      Varchar (20) NOT NULL ,
-        password      Varchar (20) NOT NULL ,
-        email         Varchar (20) NOT NULL ,
-        nom           Varchar (20) NOT NULL ,
-        prenom        Varchar (20) NOT NULL ,
-        tel           Varchar (15) NOT NULL ,
-        adresse       Varchar (50) NOT NULL ,
-        quotient_fam  Int NOT NULL ,
-        service       Enum ("comptabilite","developpeur","commercial","ressources_humaines") NOT NULL ,
-        sexe          Enum ("homme","femme") NOT NULL
-	,CONSTRAINT admin_PK PRIMARY KEY (idutilisateur)
+		FOREIGN KEY (idutilisateur) REFERENCES salarie(idutilisateur)
+);
 
-	,CONSTRAINT admin_salarie_FK FOREIGN KEY (idutilisateur) REFERENCES salarie(idutilisateur)
-)ENGINE=InnoDB;
+
+insert into admin values (1);
 
 
 #------------------------------------------------------------
@@ -138,14 +121,13 @@ CREATE TABLE admin(
 
 CREATE TABLE contact(
         id_contact    Int  Auto_increment  NOT NULL ,
-        objet         Varchar (50) NOT NULL ,
-        contenu       Varchar (500) NOT NULL ,
-        date          Date NOT NULL ,
-        idutilisateur Int NOT NULL
-	,CONSTRAINT contact_PK PRIMARY KEY (id_contact)
-
-	,CONSTRAINT contact_utilisateur_FK FOREIGN KEY (idutilisateur) REFERENCES utilisateur(idutilisateur)
-)ENGINE=InnoDB;
+        objet         Varchar (50),
+        contenu       Varchar (500),
+        date          Date,
+        idutilisateur Int NOT NULL,
+		PRIMARY KEY (id_contact),
+		FOREIGN KEY (idutilisateur) REFERENCES utilisateur(idutilisateur)
+);
 
 
 #------------------------------------------------------------
@@ -155,10 +137,8 @@ CREATE TABLE contact(
 CREATE TABLE participer(
         idutilisateur    Int NOT NULL ,
         id_activite      Int NOT NULL ,
-        date_inscription Date NOT NULL
-	,CONSTRAINT participer_PK PRIMARY KEY (idutilisateur,id_activite)
-
-	,CONSTRAINT participer_salarie_FK FOREIGN KEY (idutilisateur) REFERENCES salarie(idutilisateur)
-	,CONSTRAINT participer_activite0_FK FOREIGN KEY (id_activite) REFERENCES activite(id_activite)
-)ENGINE=InnoDB;
-
+        date_inscription Date,
+		PRIMARY KEY (idutilisateur,id_activite),
+		FOREIGN KEY (idutilisateur) REFERENCES salarie(idutilisateur),
+		FOREIGN KEY (id_activite) REFERENCES activite(id_activite)
+);
