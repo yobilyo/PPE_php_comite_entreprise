3210e3a65cc209d4ac5360e3cbf1c5f1f2a3555c
yobilyo <bbb.yousfi@gmail.com>
2020-12-05 10:31:54 +0100

    bootstrap files locally hosted

--- index.php
+++ index.php
@@ -11,10 +11,13 @@
 <head>
     <meta charset="utf-8">
     <title>Comité d'entreprise</title>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!-- jquery 3.5.1 -->
+    <script src="lib/js/jquery-3.5.1.slim.min.js"></script>
+    <!-- popper.js 1.16.1 -->
+    <script src="lib/js/popper.min.js"></script>
+    <!-- bootstrap 4.5.3 -->
+    <link rel="stylesheet" href="lib/css/bootstrap.min.css">
+    <script src="lib/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="lib/css/style.css">
     
     <script src="lib/js/helpers.js"></script>
