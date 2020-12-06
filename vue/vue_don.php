1c43b0e4a9302909a88af960b12d1c875f1e25f6
ti_Ju92 <mrspotify97@gmail.com>
2020-12-06 16:43:25 +0100

    get don

--- vue/vue_don.php
+++ vue/vue_don.php
@@ -3,7 +3,8 @@
 	<table class="table table-striped">
 		<thead>
 			<tr>
-			<th> ID DON </th>
+				<th> ID DON </th>
+				<th> ID utilisateur </th>
 				<th> Email </th>
 				<th> Société</th> 
 				<th> Budget </th> 	
@@ -18,6 +19,7 @@
 			foreach ($lesDons as $leDon) {
 				echo "<tr> 
 						<td>".$leDon['iddon']." </td>
+						<td>".$leDon['idutilisateur']. "</td>
 						<td>".$leDon['email']." </td>
 						<td>".$leDon['societe']." </td>
 						<td>".$leDon['budget']." </td>
@@ -26,10 +28,10 @@
 					
 					
 						<td>
-						<a href='index.php?page=8&action=sup&iddon=".$leDon['iddon']."'>
+						<a href='index.php?page=8&action=sup&iddon=".$leDon['iddon']."&idutilisateur=".$leDon['idutilisateur']."'>
 						<img src ='lib/images/sup.png' height='30' witdh='30'> </a>
 
-						<a href='index.php?page=8&action=edit&iddon=".$leDon['iddon']."'>
+						<a href='index.php?page=8&action=edit&iddon=".$leDon['iddon']."&idutilisateur=".$leDon['idutilisateur']."'>
 						<img src ='lib/images/edition.png' height='30' witdh='30'> </a>
 
 						</td>
