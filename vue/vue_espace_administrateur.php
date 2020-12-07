ab544aa30f8eafac7a66bc95cc30b2d870d95096
yobilyo <bbb.yousfi@gmail.com>
2020-12-07 09:55:16 +0100

    ajout image espace administrateur

--- vue/vue_espace_administrateur.php
+++ vue/vue_espace_administrateur.php
@@ -1,4 +1,6 @@
 <?php
+    echo "<br/>
+    <img src='lib/images/pages/admin.png' width='150'></img>";
     echo "<h2> Liste des administrateurs du CE </h2>
     <br/>";
     require_once("vue/vue_administrateur.php");
