685dc10cd9667fb0b62f01c7b196ac6ce467146c
yobilyo <bbb.yousfi@gmail.com>
2020-12-07 08:26:13 +0100

    ajout image tresorerie

--- vue/vue_tresorerie.php
+++ vue/vue_tresorerie.php
@@ -1,19 +1,27 @@
 <div class='container'>
 	<h2> Situation de la trésorerie </h2>
-	<table class="table table-striped">
+	<br/>
+	<!--<table class="table table-striped">
 		<thead>
 			<tr>
 				<th> Fonds actuelle </th>
 			</tr>
 		</thead>
+	-->
 
 		<tbody>
+			<img src='lib/images/tresorerie.png' width='300'></img>
+			<br/>
 			<?php 
-			foreach ($lesTresoreries as $uneTresorerie) {
+			$uneTresorerie = $lesTresoreries[0];
+			$uneTresorerieFonds = $uneTresorerie['fonds'];
+			echo "<h3>".$uneTresorerieFonds." €</h3>";
+			
+			/*foreach ($lesTresoreries as $uneTresorerie) {
 				echo "<tr> 
 						<td>".$uneTresorerie['fonds']."€ </td>
 					</tr>";
-			}
+			}*/
 			?>
 		</tbody>
 	</table>
